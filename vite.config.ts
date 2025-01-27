31a1fa3e2e8e3f65d6c7e55f4acf6a39df9f84d3
Daniel <daniel.montiel@unp.gov.co>
2025-01-27 12:32:40 -0500

    Inicialización de la librería React del Ecosistema de Información de la UNP

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import { resolve } from 'path'
+import dtsPlugin from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dtsPlugin({ include: ['lib'] })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    }
+  },
+})
