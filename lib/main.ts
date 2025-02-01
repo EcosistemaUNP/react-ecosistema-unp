2bd85c07a1995dac841feaf31a835c0fc08b46e9
Daniel <daniel.montiel@unp.gov.co>
2025-02-01 14:15:19 -0500

    Estructuración de rutas de exportación

--- lib/main.ts
+++
@@ -1,3 +0,0 @@
-export { VentanaLienzo } from './shared/components/VentanaLienzo'
-
-export { AuthProvider } from './auth/contexts/AuthContex'
---
+++ lib/shared.ts
@@ -0,0 +1,1 @@
+export { VentanaLienzo } from './shared/components/VentanaLienzo'
