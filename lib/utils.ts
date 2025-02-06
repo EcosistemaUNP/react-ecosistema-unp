73e625ae8f34b3c1abfb53da89ce961deda1e761
Daniel <daniel.montiel@unp.gov.co>
2025-02-06 15:48:12 -0500

    v0.7.0 - Se agregan componentes ProtectedRoute y UserRoute

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,3 @@
 export { AuthProvider } from './auth/contexts/AuthContex.js'
+export { ProtectedRoute } from './utils/components/ProtectedRoute.js'
+export { UserRoute } from './utils/components/UserRoute.js'
