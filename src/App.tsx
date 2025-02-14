79acdf189048d4b8db1d47e55cc2476de85a0a17
Daniel <daniel.montiel@unp.gov.co>
2025-02-14 01:44:42 -0500

    Se mueven react y react-dom a peerDependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Route, Routes } from 'react-router-dom'
 import { Ventana } from './pages/Ventana'
 import { Tabs } from './pages/Tabs'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Ventana />} />
-      <Route path='/tabs' element={<Tabs />} />
-    </Routes>
+    <Ventana />
+    // <Tabs />
   )
 }
 
