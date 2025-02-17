d3f5c4c50870401df3994f95b317b4315b80b456
Daniel <daniel.montiel@unp.gov.co>
2025-02-17 11:45:46 -0500

    Docs funcionales de la tarjeta de lectura

--- src/stories/card/tarjeta-lectura/TarjetaLectura.stories.tsx
+++ src/stories/card/tarjeta-lectura/TarjetaLectura.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ContenedorTarjeta, SeccionTarjetaLectura, TarjetaLectura } from '../../../../lib/cards';
 import { Col, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap';
 import { Subtitulo } from '../../../../lib/ui';
-import { FaClipboard, FaLocationDot, FaMap, FaPhone, FaUser } from 'react-icons/fa6';
+import { FaClipboard, FaFaceGrin, FaLocationDot, FaMap, FaPhone, FaUser } from 'react-icons/fa6';
 
 const meta: Meta<typeof TarjetaLectura> = {
     title: 'Tarjetas/Tarjeta lectura/TarjetaLectura',
@@ -62,25 +62,36 @@ export const TarjetaMúltiplesPáginas: Story = {
                 icon: FaPhone,
                 label: 'Contacto',
                 content:
-                    <SeccionTarjetaLectura title='Título de la sección'>
-                        <ContenedorTarjeta>
-                            Integer suscipit magna felis, at efficitur nulla aliquam vitae. Donec ac felis justo. Mauris ac odio non nunc placerat molestie. Nam ac justo a quam tincidunt tempor nec in tellus. Sed lacinia massa id mauris convallis gravida. In sodales tristique diam, hendrerit suscipit leo aliquet non. Praesent libero lacus, sagittis sed tincidunt vitae, tincidunt ac diam. Vestibulum rhoncus, tortor ac tincidunt suscipit, nisl purus semper risus, vel placerat diam magna porttitor turpis. Aliquam aliquam, urna ac porta tincidunt, mi ligula molestie orci, a luctus ex velit eget tortor. Nunc volutpat eros turpis, tincidunt pharetra nibh lacinia vitae.
-                        </ContenedorTarjeta>
-                        <Row className="mb-3">
-                            <FormGroup as={Col} md="4" controlId="validationCustom01">
-                                <FormLabel>Input</FormLabel>
-                                <FormControl type="text" />
-                            </FormGroup>
-                            <FormGroup as={Col} md="4" controlId="validationCustom02">
-                                <FormLabel>Input</FormLabel>
-                                <FormControl type="text" />
-                            </FormGroup>
-                            <FormGroup as={Col} md="4" controlId="validationCustomUsername">
-                                <FormLabel>Input</FormLabel>
-                                <FormControl type="text" />
-                            </FormGroup>
-                        </Row>
-                    </SeccionTarjetaLectura>
+                    <>
+                        <SeccionTarjetaLectura title='Título de la sección'>
+                            <ContenedorTarjeta>
+                                Integer suscipit magna felis, at efficitur nulla aliquam vitae. Donec ac felis justo. Mauris ac odio non nunc placerat molestie. Nam ac justo a quam tincidunt tempor nec in tellus. Sed lacinia massa id mauris convallis gravida. In sodales tristique diam, hendrerit suscipit leo aliquet non. Praesent libero lacus, sagittis sed tincidunt vitae, tincidunt ac diam. Vestibulum rhoncus, tortor ac tincidunt suscipit, nisl purus semper risus, vel placerat diam magna porttitor turpis. Aliquam aliquam, urna ac porta tincidunt, mi ligula molestie orci, a luctus ex velit eget tortor. Nunc volutpat eros turpis, tincidunt pharetra nibh lacinia vitae.
+                            </ContenedorTarjeta>
+                            <Row className="mb-3">
+                                <FormGroup as={Col} md="4" controlId="validationCustom01">
+                                    <FormLabel>Input</FormLabel>
+                                    <FormControl type="text" />
+                                </FormGroup>
+                                <FormGroup as={Col} md="4" controlId="validationCustom02">
+                                    <FormLabel>Input</FormLabel>
+                                    <FormControl type="text" />
+                                </FormGroup>
+                                <FormGroup as={Col} md="4" controlId="validationCustomUsername">
+                                    <FormLabel>Input</FormLabel>
+                                    <FormControl type="text" />
+                                </FormGroup>
+                            </Row>
+                        </SeccionTarjetaLectura>
+                        <SeccionTarjetaLectura title='Título de la sección'>
+                            <Subtitulo icon={FaFaceGrin} subtitle='Subtítulo' />
+                            <p>
+                                Vivamus imperdiet facilisis dui commodo euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et augue est. Etiam non metus laoreet, pellentesque lorem at, vehicula odio. Praesent quis aliquam justo, sit amet fringilla neque. Sed ut ipsum at arcu fermentum vestibulum. Etiam scelerisque ut quam ac mattis. Phasellus eleifend hendrerit nibh, in pretium leo tincidunt ac. Praesent egestas neque sem, a tristique felis facilisis eu. Fusce vulputate lacus id elit luctus porttitor. Sed aliquam sollicitudin mattis. Mauris id volutpat enim. Etiam placerat porta semper. Curabitur accumsan lobortis eleifend.
+                            </p>
+                            <p>
+                                Vestibulum vel lorem eget lorem egestas tempus ac sed arcu. Mauris euismod rhoncus mi, sed iaculis magna. Vivamus nec bibendum felis. Vestibulum et feugiat erat. Aliquam mattis venenatis pellentesque. Mauris quis aliquam augue. Nam rutrum sit amet leo aliquam vulputate. Proin non volutpat est. Proin non ornare ligula. Curabitur eleifend, nibh eget varius semper, sem enim fermentum libero, ac porta lacus ante a turpis. Phasellus non facilisis libero.
+                            </p>
+                        </SeccionTarjetaLectura>
+                    </>
             },
             {
                 icon: FaLocationDot,
